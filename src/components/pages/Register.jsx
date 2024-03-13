3620fc051cbb8c03e2a376343538c0f1b46860a3
Desiree <desireesanchezdev@gmail.com>
2024-03-13 19:44:54 +0100

    Created Register Page

---
+++ src/components/pages/Register.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FormRegisterComponent from '../form-register-component/FormRegisterComponent'
+
+const Register = () => {
+  return (
+    <FormRegisterComponent/>
+  )
+}
+
+export default Register
