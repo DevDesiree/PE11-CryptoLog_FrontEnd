241762befb80a80a1c5cecba7c1b60f18817f215
Desiree <desireesanchezdev@gmail.com>
2024-03-14 22:26:32 +0100

    Created Login Page and added on router

--- src/configuration/Router.jsx
+++ src/configuration/Router.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Register from '../components/pages/Register'
+import Login from '../components/pages/Login'
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/register' element={<Register />} />
+                <Route path='/login' element={<Login />} />
             </Routes>
         </BrowserRouter>
     )
