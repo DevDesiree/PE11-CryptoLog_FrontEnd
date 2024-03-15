0663ee5337be2c5ed635360b7588b1d462ccbbea
Desiree <desireesanchezdev@gmail.com>
2024-03-15 10:51:00 +0100

    Added functions on Router for check authentication users

--- src/configuration/Router.jsx
+++ src/configuration/Router.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Register from '../components/pages/Register'
-import Login from '../components/pages/Login'
-import Home from '../components/pages/Home'
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Register from '../components/pages/Register';
+import Login from '../components/pages/Login';
+import Home from '../components/pages/Home';
+import NavbarComponent from '../components/navbar-component/NavbarComponent';
 
 const Router = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const handleLogin = () => {
+        setIsAuthenticated(true);
+        console.log("Usuario autenticado:", isAuthenticated);
+    };
+
+    const handleLogout = () => {
+        setIsAuthenticated(false);
+        console.log("Usuario NO autenticado");
+      
+    };
+
     return (
         <BrowserRouter>
+            <NavbarComponent isAuthenticated={isAuthenticated} handleLogout={handleLogout} />
             <Routes>
-                <Route path='/register' element={<Register />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/' element={<Home />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={isAuthenticated ? <Home /> : <Register handleLogin={handleLogin}/>} />
+                <Route path="/login" element={isAuthenticated ? <Home /> : <Login handleLogin={handleLogin} />} />
             </Routes>
         </BrowserRouter>
-    )
+    );
 }
 
-export default Router
+export default Router;
