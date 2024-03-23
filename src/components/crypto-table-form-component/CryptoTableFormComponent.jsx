bbfe37690f538501eea6c6f34409d6ac6c3c0966
Desiree <desireesanchezdev@gmail.com>
2024-03-23 16:43:08 +0100

    Added functionality for Show, added and remove favorites on CryptoTableForm

--- src/components/crypto-table-form-component/CryptoTableFormComponent.jsx
+++ src/components/crypto-table-form-component/CryptoTableFormComponent.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import ApiCacheJson from '../../services/ApiCacheJson';
+import BackendFetchApi from '../../services/BackendFetchApi';
 
 const CryptoTableFormComponent = ({ isAuthenticated }) => {
 
@@ -7,31 +9,63 @@ const CryptoTableFormComponent = ({ isAuthenticated }) => {
     const noIconFavorite = <svg className="w-6 h-6 fill-current text-gray-400 mr-1" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" /></svg>
 
     const [searchTerm, setSearchTerm] = useState('');
-    const [favorites, setFavorites] = useState({});
+    const [favorites, setFavorites] = useState([]);
+    const [cryptoData, setCryptoData] = useState([]);
 
     const handleChange = (e) => {
         setSearchTerm(e.target.value);
     };
 
-    const handleFavoriteClick = (id) => {
+    const handleFavoriteSubmit = async (e, id) => {
+        e.preventDefault();
+
         if (isAuthenticated) {
-            setFavorites(prevState => ({
-                ...prevState,
-                [id]: !prevState[id]
-            }));
+            try {
+                if (favorites.includes(id)) {
+                    await BackendFetchApi.removeFavoriteCoin(id);
+                    console.log(`Se ha eliminado ${id}`);
+                } else {
+                    await BackendFetchApi.addFavoriteCoin(id);
+                    console.log(`Se ha agregado ${id}`);
+                }
+
+                const updatedFavorites = favorites.includes(id) ? favorites.filter(favoriteID => favoriteID !== id) : [...favorites, id];
+                setFavorites(updatedFavorites);
+            } catch (error) {
+                console.error('Error al agregar/eliminar moneda favorita:', error);
+            }
         } else {
             alert('Por favor inicia sesión para agregar criptomonedas a favoritos.');
         }
     };
+    useEffect(() => {
+        const fetchCryptocurrencies = async () => {
+            try {
+                const response = await ApiCacheJson.getCryptocurrencies();
+                setCryptoData(response);
+            } catch (error) {
+                console.error('Error al cargar las criptomonedas:', error);
+            }
+        };
 
-    // Array de ejemplo para probar
-    const cryptoData = [
-        { id: 1, name: "Bitcoin", symbol: "BTC", price: 60000, icon: "bitcoin-icon.svg" },
-        { id: 2, name: "Ethereum", symbol: "ETH", price: 2000, icon: "ethereum-icon.svg" },
-        { id: 3, name: "Litecoin", symbol: "LTC", price: 150, icon: "litecoin-icon.svg" },
-    ];
+        fetchCryptocurrencies();
+    }, []); // Se ejecuta solo una vez al montar el componente
 
-    // Filtrando las monedas basándose en el término de búsqueda
+    useEffect(() => {
+        const fetchUserFavorites = async () => {
+            try {
+                const userFavorites = await BackendFetchApi.getFavoriteCoin();
+                setFavorites(userFavorites);
+            } catch (error) {
+                console.error('Error al obtener los favoritos del usuario:', error);
+            }
+        };
+
+        if (isAuthenticated) {
+            fetchUserFavorites();
+        }
+    }, [isAuthenticated]);
+
     const filteredCryptoData = cryptoData.filter((crypto) => {
         return crypto.name.toLowerCase().includes(searchTerm.toLowerCase()) || crypto.symbol.toLowerCase().includes(searchTerm.toLowerCase());
     });
@@ -85,23 +119,24 @@ const CryptoTableFormComponent = ({ isAuthenticated }) => {
                             <tr key={crypto.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                 <td className="px-6 py-4">
                                     <div>
-                                        <button onClick={() => handleFavoriteClick(crypto.id)} className="p-2 rounded-full">
-                                            {favorites[crypto.id] ? iconFavorite : noIconFavorite}
-                                        </button>
-
+                                        <form onSubmit={(e) => handleFavoriteSubmit(e, crypto.id)}>
+                                            <button type="submit" className="p-2 rounded-full">
+                                                {favorites.includes(crypto.id) ? iconFavorite : noIconFavorite}
+                                            </button>
+                                        </form>
                                     </div>
                                 </td>
                                 <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    {crypto.id}
+                                    {crypto.market_cap_rank}
                                 </td>
                                 <td className="px-6 py-4">
                                     {crypto.name}
                                 </td>
                                 <td className="px-6 py-4">
-                                    {crypto.symbol}
+                                    {crypto.symbol.toUpperCase()}
                                 </td>
                                 <td className="px-6 py-4">
-                                    ${crypto.price}
+                                    {crypto.current_price}$
                                 </td>
                             </tr>
                         ))}
