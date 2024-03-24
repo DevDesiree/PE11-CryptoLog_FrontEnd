51a8b4c28b5a42a1bbbe1cb89ea5eb6d22c34d98
Desiree <desireesanchezdev@gmail.com>
2024-03-24 19:11:52 +0100

    Added funcionality for preview image and send image to cloudinary

--- src/components/pages/UserProfile.jsx
+++ src/components/pages/UserProfile.jsx
@@ -23,12 +23,36 @@ const UserProfile = ({ isAuthenticated }) => {
         };
 
         fetchUserProfile();
-    }, []); // El segundo argumento [] se asegura que useEfect solo se ejecute una vez al montar el componente
+    }, []);
 
     const handleChange = (e) => {
-        setUserData({ ...userData, [e.target.name]: e.target.value });
+        const { name, value, files } = e.target;
+        if (files && files.length > 0) {
+            const selectedFile = files[0];
+    
+            // Lee el archivo seleccionado y obtiene su URL
+            const reader = new FileReader();
+            reader.onload = function(event) {
+                const fileUrl = event.target.result;
+    
+                // Actualiza el estado con la URL del archivo
+                setUserData(prevState => ({
+                    ...prevState,
+                    avatar: selectedFile,
+                    avatarUrl: fileUrl // Agrega la URL del archivo al estado
+                }));
+            };
+            reader.readAsDataURL(selectedFile); // Lee el archivo como una URL de datos
+    
+        } else {
+            setUserData(prevState => ({
+                ...prevState,
+                [name]: value
+            }));
+        }
     };
-
+    
+    
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
@@ -38,6 +62,7 @@ const UserProfile = ({ isAuthenticated }) => {
             console.error('Error al actualizar el perfil del usuario:', error);
         }
     };
+    
     return (
         <div>
             {isAuthenticated ? (
@@ -46,8 +71,10 @@ const UserProfile = ({ isAuthenticated }) => {
                     <form onSubmit={handleSubmit} className="max-w-5xl mx-auto rounded-lg p-6  shadow-2xl sm:rounded-3xl border-gray-600 my-7 flex flex-col items-center">
                         <div className="flex flex-col sm:flex-row w-full">
                             <div className="flex flex-col w-full sm:w-1/2 pr-10 items-center mb-8 sm:mb-0">
-                                <h2 className="text-center text-xl text-white ">Tu Avatar</h2>
-                                <img className="rounded-full w-60 h-60 mb-5" src={userData.avatar} alt="Avatar" />
+                                <h2 className="text-center text-xl text-white mb-5">Tu Avatar</h2>
+                                <img className="rounded-full w-60 h-60 mb-5" src={userData.avatarUrl || userData.avatar} alt="Avatar" />
+
+
                                 <label htmlFor="avatar" className="block text-sm font-medium text-white text-center">Cambiar Avatar</label>
                                 <input type="file" id="avatar" name="avatar" className="block w-full p-3 mb-3 mt-2 text-sm  border rounded-lg cursor-pointer text-gray-400 focus:outline-none bg-gray-500 bg-opacity-20 border-gray-600 placeholder-gray-400" onChange={handleChange} />
                             </div>
