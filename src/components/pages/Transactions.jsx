c53fca3ccb3016f11e567f64e1d541f0cd8cd67b
Desiree <desireesanchezdev@gmail.com>
2024-03-24 21:32:18 +0100

    Added banner on Transactions Page

--- src/components/pages/Transactions.jsx
+++ src/components/pages/Transactions.jsx
@@ -6,7 +6,12 @@ import UpdateTransactionModalComponent from '../update-transaction-modal-compone
 const Transactions = ({ isAuthenticated }) => {
   return (
     <>
-      <CreateTransactionModalComponent isAuthenticated={isAuthenticated}/>
+      <section className="h-[150px]  bg-no-repeat bg-cover bg-[url('/src\assets\images\banner-mis-transacciones.jpg')] bg-gray-700 bg-blend-multiply mb-5">
+        <div className="px-4 mx-auto max-w-screen-lg text-center py-10 lg:py-30">
+          <h1 className="mb-4 text-3xl font-extrabold tracking-tight leading-none text-white md:text-4xl lg:text-5xl">Mis Activos</h1>
+        </div>
+      </section>
+      <CreateTransactionModalComponent isAuthenticated={isAuthenticated} />
       <UpdateTransactionModalComponent isAuthenticated={isAuthenticated} />
       <TransactionTableComponent isAuthenticated={isAuthenticated} />
     </>
